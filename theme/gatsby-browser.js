056bf1dab9c9aa56944b64acaec4e9b2408f3312
Ben Ely <ben.ely@h4bmanchester.com>
2019-08-03 13:04:50 +0100

    Switch to HOC component for authentication

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
 import { AuthenticatorProvider } from './src/components/amplify';
 
-export const wrapPageElement = ({ element }, options) => (
-    <AuthenticatorProvider options={options}>
-        {React.cloneElement(element, { user: "top bottom" })}
-    </AuthenticatorProvider>
-)
+export const wrapPageElement = ({ element }, options) => {
+    Amplify.configure({
+        region: options.region,
+        userPoolId: options.userPoolId,
+        userPoolWebClientId: options.userPoolWebClientId
+    });
+
+    return <AuthenticatorProvider>{element}</AuthenticatorProvider>
+}
--- theme/src/components/amplify/authenticator-provider.js
+++ theme/src/components/amplify/authenticator-provider.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react';
-import Amplify from 'aws-amplify';
-import { Authenticator } from 'aws-amplify-react';
+import React from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 
-class AuthenticatorProvider extends Component {
-    componentDidMount() {
-        Amplify.configure({
-            region: this.props.options.region,
-            userPoolId: this.props.options.userPoolId,
-            userPoolWebClientId: this.props.options.userPoolWebClientId
-        });
-    }
+const AuthenticatorProvider = (props) => (
+    React.cloneElement(props.children, { authState: props.authState, authData: props.authData })
+);
 
-    render() {
-        return (
-            <Authenticator hideDefault={true}>
-                {React.cloneElement(this.props.children, {
-                    userStatus: this.props.authState,
-                    userData: this.props.authData
-                })}
-            </Authenticator>
-        )
-    }
-}
+export default withAuthenticator(AuthenticatorProvider, {
 
-export default AuthenticatorProvider;
+});
