598a1d82cde18417a174887a49c9978bbe172dee
Hoang <lhhoang@IfiSolution.local>
2021-12-07 00:20:19 +0700

    improve doAuthenticate, work with any paths that include element of doAuthenticate array

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -10,49 +10,48 @@ export const wrapPageElement = ({ element, props }, options) => {
     identityPoolId: options.identityPoolId,
   })
 
+  // `doNotAuthenticate` and `doAuthenticate` cannot present at the same time!
   if (
     options.hasOwnProperty('doNotAuthenticate') == true &&
     options.hasOwnProperty('doAuthenticate') == true
   ) {
-    // console.log("`doNotAuthenticate` and `doAuthenticate` can not present at the same time!")
+    // console.log("`doNotAuthenticate` and `doAuthenticate` cannot present at the same time!")
     return <AuthenticatorProvider>{element}</AuthenticatorProvider>
   }
 
+  // `doNotAuthenticate` setting only
   if (
     options.hasOwnProperty('doAuthenticate') == false &&
     options.hasOwnProperty('doNotAuthenticate') == true
   ) {
-      // console.log("`doNotAuthenticate` setting only")
       if (
         options.doNotAuthenticate.includes(props.path)
       ) {
-          // console.log("doNotAuthenticate included this path")
           return element
       }
-      // console.log("doNotAuthenticate is not including this path")
       return <AuthenticatorProvider>{element}</AuthenticatorProvider>
   }
 
+  // `doAuthenticate` setting only
   if (
     options.hasOwnProperty('doAuthenticate') == true &&
     options.hasOwnProperty('doNotAuthenticate') == false
   ) {
-    // console.log("`doAuthenticate` setting only")
-    if (
-      options.doAuthenticate.includes(props.path)
-    ) {
-      // console.log("doAuthenticate included this path")
-      return <AuthenticatorProvider>{element}</AuthenticatorProvider>
+    for (const opt of options.doAuthenticate) {
+      if (
+        props.path.includes(opt)
+      ) {
+        return <AuthenticatorProvider>{element}</AuthenticatorProvider>
+      }
     }
-    // console.log("doAuthenticate is not including this path")
     return element
   }
 
+  // `doNotAuthenticate` and `doAuthenticate` is not configured
   if (
     options.hasOwnProperty('doNotAuthenticate') == false &&
     options.hasOwnProperty('doAuthenticate') == false
   ) {
-    // console.log("`doNotAuthenticate` and `doAuthenticate` is not configured")
     return element
   }
 
